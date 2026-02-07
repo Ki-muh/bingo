032226351a1f46ef84a7ad9d7da1750e95b00de5
Ki-muh <martinkimu09@gmail.com>
2026-02-07 17:10:29 +0300

    Added canvas in JS

--- bingo.js
+++ bingo.js
@@ -8,3 +8,16 @@ function initAll() {
         document.getElementById("sq-"+i).innerHTML = newNumber;
     }
 }
+
+// Add event listener where I manipulate all cells except the FREE cell
+window.addEventListener("load", function() {
+    // select all the td cells but the #free
+    const cells = this.document.querySelectorAll("td:not(#free)");
+
+    // of all selected cells, add a new element called canvas and append it
+    cells.forEach(cell => {
+        const canvas = this.document.createElement("canvas");
+        canvas.className = "scratch";
+        cell.appendChild(canvas);
+    })
+})
